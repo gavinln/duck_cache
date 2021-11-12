afd34140ddc2e389cfef06a9dfc7dd40dcbba4e6
Gavin Noronha <gavinln@hotmail.com>
2021-11-12 06:52:25 -0800

    add code to save and retrieve dataframes from duckdb

--- python/duck_cache.py
+++ python/duck_cache.py
@@ -3,6 +3,7 @@ cache dataframes using duckdb
 """
 import pathlib
 import logging
+import random
 
 import pandas as pd
 import duckdb
@@ -35,26 +36,50 @@ conn.execute('select * from temp_table').df()
 '''
 
 
-def save_df(conn, df):
-    "save dataframe to a duckdb"
-    breakpoint()
-    conn.register("temp_df", df)
-    conn.execute("create table table_df as select * from temp_df")
-    conn.unregister("temp_df")
+def get_random_table_name():
+    return 'table_{}'.format(random.randrange(int(1e5), int(1e6)))
 
 
-def show_tables(conn):
+def get_df(conn, name: str):
+    "get duckdb table as a dataframe"
+    sql = f'select * from {name}'
+    return conn.execute(sql).fetchdf()
+
+
+def get_tables(conn):
+    'get list of tables in duckdb'
     tables = []
     for table_tuple in conn.execute("PRAGMA show_tables").fetchall():
         tables.append(table_tuple[0])
     return tables
 
 
+def get_new_table_name(conn):
+    'get new name for table which does not exist'
+    while True:
+        table_name = get_random_table_name()
+        if table_name not in get_tables(conn):
+            return table_name
+
+
+def save_df(conn, df, name):
+    "save dataframe to a duckdb"
+    temp_table = get_new_table_name(conn)
+    conn.register(temp_table, df)
+    sql = "create table {} as select * from {}".format(
+        name, temp_table)
+    conn.execute(sql)
+    conn.unregister(temp_table)
+
+
 def main() -> None:
     conn = get_db_conn()
-    df1 = pd.DataFrame([[1, 2], [4, 4]], columns=[list("ab")])
-    save_df(conn, df1)
-    print(show_tables(conn))
+    df = pd.DataFrame([[1, 2], [4, 4]], columns=list('ab'))
+    table_name = 'table1'
+    save_df(conn, df, table_name)
+    save_df(conn, df, 'table2')
+    print(get_tables(conn))
+    print(get_df(conn, table_name))
 
 
 if __name__ == "__main__":
